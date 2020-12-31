10ad50cbc7fab187dbda653b2298c03f782ab787
Cathella <ncathie214@gmail.com>
2020-12-31 15:05:43 +0300

    scaffold clients, projects

---
+++ db/migrate/20201231115714_create_projects.rb
@@ -0,0 +1,14 @@
+class CreateProjects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :projects do |t|
+      t.string :ref_id
+      t.string :description
+      t.string :site_measurement
+      t.string :progress
+      t.string :type
+      t.string :cost
+
+      t.timestamps
+    end
+  end
+end
